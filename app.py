0a86b3ed57049833f9b3f073bdc30770587996d3
mcgourtyalex <mcgourty.alex@gmail.com>
2015-01-15 12:05:25 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,82 @@
+import json, urllib, math
+from flask import Flask, jsonify, make_response
+
+app = Flask(__name__)
+
+# read truck data
+url = "https://data.sfgov.org/resource/rqzj-sfat.json"
+response = urllib.urlopen(url);
+data = json.loads(response.read())
+
+trucks = []
+seen = set()
+
+# keep only relevant truck data:
+#   remove incomplete data
+#   remove requested applicants
+#   prevent duplicates from being shown
+
+for row in data:
+    if ('address' not in row or 'status' not in row or 'approved' not in row or 'location' not in row):
+        continue
+    if (row['status'] == 'REQUESTED'):
+        continue;
+    if (row['applicant'] in seen):
+        continue;
+    seen.add(row['applicant'])
+    temp = {}    
+    
+    temp['address'] = row['address']
+    temp['date'] = row['approved']
+    temp['location'] = row['location']
+    temp['applicant'] = row['applicant']
+    temp['food'] = row['fooditems']
+
+    trucks.append(temp);
+
+
+# get the distance between two points on the map
+def distance(lat1,long1,lat2,long2):
+    return math.sqrt(math.pow((lat1 - lat2),2) + math.pow((long1 - long2),2))
+
+# get /trucks will return the truck list
+@app.route('/trucks', methods=['GET'])
+def get_trucks():
+    return jsonify({'trucks': trucks})
+    
+# get /closest/lat/long will return the closest 20 trucks to the point given
+@app.route('/closest/<string:lat>/<string:lon>', methods=['GET'])
+def get_closest(lat,lon):
+    if (lat == "" or lon == ""):
+        return
+        
+    lat1 = float(lat)
+    lon1 = float(lon)
+    
+    distances = []
+    
+    i = 0
+    for truck in trucks:
+        lat2 = float(truck['location']['latitude'])
+        lon2 = float(truck['location']['longitude'])
+        
+        dist = distance(lat1,lon1, lat2, lon2)
+        distances.append((dist,i))
+        i += 1
+    
+    sort = sorted(distances)
+    
+    topTwenty = []
+    
+    for num in sort[0:20]:
+        topTwenty.append(trucks[num[1]])
+     
+    return jsonify({'min': topTwenty})
+    
+# handle nonsense requests
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+	
+	var lastOpened;
+	
+	//get truck list asynchronously
+	//populate map after truck location data returned:
+	$.getJSON( "http://localhost:5000/trucks", function(data) {
+		
+		// initialize the map
+		function initialize() {
+			// center in SF
+			var options = {
+				center: {lat: 37.7833, lng: -122.4167},
+				zoom: 13,
+				streetViewControl: false,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			// create new map
+			var map = new google.maps.Map(document.getElementById('map-canvas'), options);
+			
+			// populate sidebar
+			populate_list(map.getCenter());
+			
+			// add markers for each truck
+			for (truck in data.trucks) {
+				var loc = data.trucks[truck].location;
+				var title = data.trucks[truck].applicant;
+				console.log(data.trucks[truck].applicant);
+				console.log(data.trucks[truck].location);
+				var latLng = new google.maps.LatLng(loc.latitude,loc.longitude);
+				createMarker();
+				
+				// create in function to avoid duplication
+				function createMarker() {
+					var marker = new google.maps.Marker({
+						position: latLng,
+						map: map,
+						title: title,
+					});
+				
+					var infowindow = new google.maps.InfoWindow({
+						content: title,
+					});
+					
+					google.maps.event.addListener(marker, 'click', function() {
+						if (lastOpened != undefined) {
+							lastOpened.close();
+						}
+						infowindow.open(map, marker);
+						map.panTo(marker.position);
+						lastOpened = infowindow;
+					});
+					marker.setMap(map);
+				}
+			}
+			
+			// add marker for current location if able
+			if(navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(function(position) {
+					var initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+					var myloc = new google.maps.Marker({
+						position: initialLocation,
+						clickable: true,
+						icon: {
+							path: google.maps.SymbolPath.CIRCLE,
+							scale: 5,
+							strokeColor: '#FF0000',
+							strokeOpacity: 0.8,
+							strokeWeight: 2,
+							fillColor: '#FF0000',
+							fillOpacity: 0.35,
+						},
+						shadow: null,
+						zIndex: 999,
+					});
+					google.maps.event.addListener(myloc, 'click', function() {
+						map.panTo(myloc.position);
+					});
+					myloc.setMap(map);
+					
+				}, function() {
+					console.log("Geolocation unavailable");
+				});
+			}
+			
+			// add listener to repopulate the sidebar when the center changes
+			google.maps.event.addListener(map, 'center_changed', function() {
+				populate_list(map.getCenter());
+			});
+		}
+		// initialize the map at end of JSON callback
+		initialize();
+	});
+});
+
+// populate the sidebar with the closest 20 trucks to the map center
+function populate_list(center) {
+	
+	// get from server 
+	$.getJSON("http://localhost:5000/closest/" + center.k + "/" + center.D, function(data) { 
+		
+		// empty the sidebar
+		$("#list-container").empty();
+		
+		// for each closest truck, append it to the sidebar
+		for (truck in data.min) {
+			var loc = data.min[truck].location;
+			var title = data.min[truck].applicant;
+			var addr = data.min[truck].address;
+			var food = data.min[truck].food;
+			console.log(addr);
+			console.log(title);
+			$("#list-container").append('<div class="listing">' + title + '<br /><span class="sub">' + addr + '</span><div class="more">' + food + '</div></div>');
+		}
+	});
+}
+
